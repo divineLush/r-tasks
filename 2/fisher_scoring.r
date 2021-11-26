a20a0ea54096c1742fd4d3a2e937ee55861cafce
divineLush <alvipath@yahoo.com>
2021-11-26 02:36:34 -0500

    refactor & cleanup

--- 2/fisher_scoring.r
+++ 2/fisher_scoring.r
@@ -2,29 +2,32 @@
 # 2.1 FISHER'S SCORING ALGORITHM
 ##########################################
 
-x = matrix(c(rep(1,10), log(c(1/128, 1/64, 1/32, 1/16, 1/8,1/4,1/2,1,2,4))), nrow = 10, ncol = 2) # x is on log scale.
-y = matrix(c(0,0,0.4,0.4,0.6,0.8,1,1,1,1), ncol = 1, byrow = FALSE)   # y is proportion
-m = 5 # m is equal for each x_i
+x = matrix(rexp(200, rate = .1), nrow = 10, ncol = 2)
+y = matrix(c(rep(1, 10)), ncol = 1, byrow = FALSE)
 
-fisher.scoring = function(y, x, start = runif(ncol(x))) {
-  m = 5
-  p = ncol(x)
-  beta = matrix(rep(0,20*p),ncol = 20)
-  score = rep(0, p)
-  theta = matrix(rep(0,20*10), ncol =20)
-  mu = matrix(rep(0,20*10), ncol = 20)
-  beta[,1] = start
+fisher_scoring = function(y, x) {
+    p = ncol(x)
+    n = 20
+    beta = matrix(rep(0, n*p), ncol = n)
+    theta = matrix(rep(0, n*10), ncol = n)
+    mu = matrix(rep(0, n*10), ncol = n)
+    beta[,1] = runif(ncol(x))
 
-  for (i in 1:19) {
-    # cannonical link function
-    theta[,i] = x %*% as.matrix(beta[,i])
-    mu[,i] = exp(as.matrix(theta[,i]))/(1+exp(as.matrix(theta[,i])))
-    w = diag(as.vector(mu[,i] * (1-mu[,i])))
-    beta[,i+1] = as.matrix(beta[,i]) + solve(t(x)%*% w %*% x) %*% t(x) %*% (y-as.matrix(mu[,i]))
-  }
-return (beta)
+    for (i in 1:(n - 1)) {
+        # canonical link function
+        theta[,i] = x %*% as.matrix(beta[,i])
+        mu[,i] = exp(as.matrix(theta[,i])) / (1 + exp(as.matrix(theta[,i])))
+        w = diag(as.vector(mu[,i] * (1 - mu[,i])))
+
+        beta_i = as.matrix(beta[,i])
+        inf = solve(t(x)%*% w %*% x) %*% t(x)
+        score = y - as.matrix(mu[,i])
+
+        beta[,i+1] = beta_i + inf %*% score
+    }
+
+    return (beta)
 }
 
-beta = fisher.scoring(y,x)
-
-print(beta)
+fisher = fisher_scoring(y,x)
+print(fisher)
