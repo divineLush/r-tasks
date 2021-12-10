109b53d22b5d6c0561b90060093d32e6ae9dd2da
divineLush <alvipath@yahoo.com>
2021-12-10 10:16:23 -0500

    fisher scoring fixes

--- 2/fisher_scoring.r
+++ 2/fisher_scoring.r
@@ -2,44 +2,43 @@
 # 2.1 FISHER'S SCORING ALGORITHM
 ##########################################
 
-x = matrix(rexp(200, rate = .1), nrow = 10, ncol = 2)
-y = matrix(c(rep(1, 10)), ncol = 1, byrow = FALSE)
+data = read.csv('./House-Price-logit.csv')
 
-fisher_scoring = function(y, x) {
-    p = ncol(x)
-    n = 20
-    beta = matrix(rep(0, n*p), ncol = n)
-    theta = matrix(rep(0, n*10), ncol = n)
-    mu = matrix(rep(0, n*10), ncol = n)
-    beta[,1] = runif(ncol(x))
+x = as.matrix(cbind(1, data['price'], data['age']))
+y = as.matrix(data['Sold'])
 
-    for (i in 1:(n - 1)) {
-        theta[,i] = x %*% as.matrix(beta[,i])
-        mu[,i] = exp(as.matrix(theta[,i])) / (1 + exp(as.matrix(theta[,i])))
-        w = diag(as.vector(mu[,i] * (1 - mu[,i])))
-
-        beta_i = as.matrix(beta[,i])
-        inf = solve(t(x) %*% w %*% x) %*% t(x)
-        score = y - as.matrix(mu[,i])
-
-        beta[,i+1] = beta_i + inf %*% score
-    }
-
-    return (beta)
+prob = function(x) {
+    return (exp(x) / (1 + exp(x)))
 }
 
-fisher = fisher_scoring(y,x)
-print(fisher)
+fisher_matrix = function(x, beta) {
+    p_x_beta = prob(x %*% beta)
+    d = as.vector(p_x_beta * (1 - p_x_beta))
+
+    return (solve(t(x) %*% diag(d) %*% x))
+}
+
+beta = matrix(rep(0, ncol(x)))
+beta_p = 0
+
+for (i in 1:10) {
+    score = (t(x) %*% (y - prob(x %*% beta)))
+
+    beta = beta + fisher_matrix(x, beta) %*% score
+    beta_p = beta
+}
+
+sigma = sqrt(as.vector(diag(fisher_matrix(x, beta))))
+
+print('sigma')
+print(sigma)
 
 writeLines("\n><> ><> ><> ><> ><> ><> ><> ><> ><> ><>\n")
 
-theta_mle = x %*% as.matrix(fisher[, 20])
-mu_mle = exp(as.matrix(theta_mle)) / (1 + exp(as.matrix(theta_mle)))
-w_mle = diag(as.vector(mu_mle * (1 - mu_mle)))
-se = sqrt(solve(t(x) %*% w_mle %*% x))
+print('beta')
+print(as.vector(beta))
 
-print(se)
+writeLines("\n><> ><> ><> ><> ><> ><> ><> ><> ><> ><>\n")
 
-dataset = data.frame(x = x, y = y)
-g = glm(y ~ x, data = dataset, family="binomial")
-print(g$coefficients)
+glm = glm(Sold ~ price + age, data = data, family = 'binomial')
+print(glm$coefficients)
